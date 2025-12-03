c0c323941de3382e4f309771a5973fe8729ccf20
Maxime <maxime.lettier@protonmail.com>
2025-12-03 09:15:08 +0100

    updated nix flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,27 @@
 {
   description = "Devshell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }: 
-    let 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs;[
-          python312
-          python312Packages.rich
-        ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in 
+        {
+          devShells.${system}.default = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              python312
+              python312Packages.rich
+            ];
 
-        shellHook = ''
-          echo -e "\e[45m AutoAmpDL dev shell activated \e[0m"
-        '';
-      };
-    };
+            shellHook = ''
+              echo -e "\e[45m AutoAmpDL dev shell activated \e[0m"
+            '';
+          };
+        }
+    );
 }
